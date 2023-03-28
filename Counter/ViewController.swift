d38cd6b075023b59b30cad38076428c6656d31c7
Anna Guryanova <minigurk@mail.ru>
2023-03-29 01:54:33 +0300

    Fix project

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,38 +10,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var storyView: UITextView!
     private var counter: Int = 0
     private let defaultText = "Значение счётчика: "
+    
+    @IBAction func increment(_ sender: Any) {
+        incrementAction()
+    }
+    
+    @IBAction func decrement(_ sender: Any) {
+        decrementAction()
+    }
+    
+    @IBAction func zeroCounter(_ sender: Any) {
+        zeroAction()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         storyView.insertText("\n")
     }
-    func getDate() -> String {
+    
+    private func getDate() -> String {
         let date = Date()
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd HH:mm:ss"
         let dateString = df.string(from: date)
         return dateString
     }
-    @IBAction func increment(_ sender: Any) {
+    
+    private func incrementAction() {
         counter += 1
         counterLabel.text = defaultText + String(counter)
-        storyView.insertText(getDate() + " значение изменено на: +1\n")
+        storyView.insertText(getDate() + " : значение изменено на: +1\n")
     }
-    @IBAction func decrement(_ sender: Any) {
+    
+    private func decrementAction() {
         if counter > 0 {
             counter -= 1
             counterLabel.text = defaultText + String(counter)
-            storyView.insertText(getDate() + " значение изменено на: -1\n")
+            storyView.insertText(getDate() + " : значение изменено на: -1\n")
+        } else {
+            storyView.insertText(getDate() + " : попытка уменьшить значение счетчика ниже 0\n")
         }
     }
-    @IBAction func zeroCounter(_ sender: Any) {
+    
+    private func zeroAction() {
         counter = 0
         counterLabel.text = defaultText + String(counter)
         storyView.insertText(getDate() + " : значение сброшено\n")
     }
-    
-    @IBOutlet weak var storyView: UITextView!
 }
 
